519cc636a71e34841038b3423f70d9d7c77e3a20
Chase Worsley <68138593+Worsleychase@users.noreply.github.com>
2025-04-07 16:16:05 -0600

    Update code.js

--- code.js
+++ code.js
@@ -1,3 +1,10 @@
 function fib(n) {
-    return n;
+    if (n === 0) return [0];
+    if (n === 1) return [0, 1];
+
+    arr = [0, 1];
+    for (let i = 2; i <= n; i++) {
+        arr[i] = arr[i - 1] + arr[i - 2];
+    }
+    return arr;
 }
